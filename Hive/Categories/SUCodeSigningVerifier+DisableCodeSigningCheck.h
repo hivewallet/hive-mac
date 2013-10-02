bb580af1dd442f5a80f577c9e74e7f0e1ad40b67
Jakub Suder <jakub.suder@gmail.com>
2013-10-02 14:55:02 +0200

    disable Sparkle code signing check

---
+++ Hive/Categories/SUCodeSigningVerifier+DisableCodeSigningCheck.h
@@ -0,0 +1,13 @@
+//
+//  SUCodeSigningVerifier.h
+//  Hive
+//
+//  Created by Jakub Suder on 02.10.2013.
+//  Copyright (c) 2013 Hive Developers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SUCodeSigningVerifier : NSObject
+
+@end
