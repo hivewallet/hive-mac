fc8f3c0fb5dfb08f162d2b14e45892581aae0e56
Jakub Suder <jakub.suder@gmail.com>
2013-10-28 10:53:33 +0100

    fixed update_xibs script
    it didn't match languages with a hyphen in the code like zh-hans

--- update_xibs.sh
+++ update_xibs.sh
@@ -2,7 +2,7 @@
 
 for file in `find Hive -name '*.xib'`; do
   strings_file=`echo $file | sed 's/\.xib/.strings/'`
-  original_file=`echo $file | sed -E 's/[[:alpha:]]+\.lproj/en.lproj/'`
+  original_file=`echo $file | sed -E 's/([[:alpha:]]|\-)+\.lproj/en.lproj/'`
   if [ "$file" != "$original_file" ]; then
     echo "Updating $file..."
     ibtool --strings-file $strings_file --write $file $original_file
