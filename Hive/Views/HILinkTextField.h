ad5f1f277bd85e3be1645379e19e6e600e9ba395
Nikolaj Schumacher <git@nschum.de>
2013-12-04 20:46:30 +0100

    Add link to more details in fee popover.

---
+++ Hive/Views/HILinkTextField.h
@@ -0,0 +1,9 @@
+/*
+ * Displays a blue underlined link that can be clicked.
+ */
+
+@interface HILinkTextField : NSTextField
+
+@property (nonatomic, copy) NSString *href;
+
+@end
---
+++ Hive/Views/HILinkTextField.m
@@ -0,0 +1,35 @@
+#import "HILinkTextField.h"
+
+@implementation HILinkTextField
+
+- (void)setStringValue:(NSString *)aString
+{
+    [super setStringValue:aString];
+    [self updateLink];
+}
+
+- (void)setHref:(NSString *)href
+{
+    _href = [href copy];
+    [self updateLink];
+}
+
+- (void)updateLink
+{
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:self.stringValue];
+    NSRange range = NSMakeRange(0, string.length);
+    [string addAttribute:NSLinkAttributeName value:_href range:range];
+    [string addAttribute:NSForegroundColorAttributeName value:[NSColor blueColor] range:range];
+    [string addAttribute:NSUnderlineStyleAttributeName value:@(NSSingleUnderlineStyle) range:range];
+    self.attributedStringValue = string;
+
+    self.allowsEditingTextAttributes = YES;
+    self.selectable = YES;
+}
+
+- (void)resetCursorRects
+{
+    [self addCursorRect:[self bounds] cursor:[NSCursor pointingHandCursor]];
+}
+
+@end
