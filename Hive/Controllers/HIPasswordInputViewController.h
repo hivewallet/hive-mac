fbcb00720eeea369c11afdc483ab4cc2654a626a
Nikolaj Schumacher <git@nschum.de>
2013-12-09 22:44:39 +0100

    Create HIPasswordInputViewController.

---
+++ Hive/Controllers/HIPasswordInputViewController.h
@@ -0,0 +1,14 @@
+//
+//  HIPasswordInputViewController.h
+//  Hive
+//
+//  Created by Nikolaj Schumacher on 2013-12-09.
+//  Copyright (c) 2013 Hive Developers. All rights reserved.
+//
+
+/*
+ Requests an input from the user.
+ */
+@interface HIPasswordInputViewController : NSViewController
+
+@end
---
+++ Hive/Controllers/HIPasswordInputViewController.m
@@ -0,0 +1,21 @@
+//
+//  HIPasswordInputViewController.m
+//  Hive
+//
+//  Created by Nikolaj Schumacher on 2013-12-09.
+//  Copyright (c) 2013 Hive Developers. All rights reserved.
+//
+
+#import "HIPasswordInputViewController.h"
+
+@interface HIPasswordInputViewController ()
+
+@end
+
+@implementation HIPasswordInputViewController
+
+- (id)init {
+    return [self initWithNibName:[self className] bundle:nil];
+}
+
+@end
