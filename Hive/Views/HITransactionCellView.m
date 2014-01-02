0edec33086798d404294414f139ea544674110b4
Nikolaj Schumacher <git@nschum.de>
2014-01-02 15:05:56 +0100

    Only show share button when mouse hovers over the row.

--- Hive/Views/HITransactionCellView.m
+++ Hive/Views/HITransactionCellView.m
@@ -8,11 +8,51 @@
 
 #import "HITransactionCellView.h"
 
+@interface HITransactionCellView ()
+
+@property (nonatomic, assign) BOOL mouseInside;
+@property (nonatomic, strong) NSTrackingArea *trackingArea;
+
+@end
+
+
 @implementation HITransactionCellView
 
 - (IBAction)shareButtonPressed:(NSButton *)sender {
     NSSharingServicePicker *sharingServicePicker = [[NSSharingServicePicker alloc] initWithItems:@[self.shareText]];
     [sharingServicePicker showRelativeToRect:sender.bounds ofView:sender preferredEdge:CGRectMaxXEdge];
+}
+
+#pragma mark - mouse handling
+
+- (void)updateTrackingAreas {
+    [super updateTrackingAreas];
+    [self removeTrackingArea:self.trackingArea];
+
+    self.trackingArea = [[NSTrackingArea alloc] initWithRect:self.bounds
+                                                     options:NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways
+                                                       owner:self
+                                                    userInfo:nil];
+    [self addTrackingArea:self.trackingArea];
+
+    NSPoint mouseLocation = [self.window mouseLocationOutsideOfEventStream];
+    self.mouseInside = NSPointInRect([self convertPoint:mouseLocation fromView:nil], self.bounds);
+
+    [super updateTrackingAreas];
+}
+
+- (void)dealloc {
+    [self removeTrackingArea:self.trackingArea];
+}
+
+- (void)mouseEntered:(NSEvent *)theEvent {
+    [super mouseEntered:theEvent];
+    self.mouseInside = YES;
+}
+
+- (void)mouseExited:(NSEvent *)theEvent {
+    self.mouseInside = NO;
+    [super mouseExited:theEvent];
 }
 
 @end
