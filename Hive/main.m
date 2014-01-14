b1c0c5e9d8dfee39f6533685106959775d53caba
Nikolaj Schumacher <git@nschum.de>
2014-01-14 22:35:28 +0100

    Change bundle ID to com.hivewallet.Hive and migrate preferences.

--- Hive/main.m
+++ Hive/main.m
@@ -8,6 +8,22 @@
 
 #import <Cocoa/Cocoa.h>
 
+void migratePreferences();
+
 int main(int argc, char *argv[]) {
+    migratePreferences();
     return NSApplicationMain(argc, (const char **)argv);
+}
+
+void migratePreferences() {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSURL *libraryDir = [fileManager URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask][0];
+    NSURL *preferencesDir = [libraryDir URLByAppendingPathComponent:@"Preferences"];
+    NSURL *oldPreferencesFile = [preferencesDir URLByAppendingPathComponent:@"com.grabhive.Hive.plist"];
+    if ([fileManager fileExistsAtPath:oldPreferencesFile.path]) {
+        NSURL *newPreferencesFile = [preferencesDir URLByAppendingPathComponent:@"com.hivewallet.Hive.plist"];
+        if (![fileManager moveItemAtURL:oldPreferencesFile toURL:newPreferencesFile error:NULL]) {
+            HILogError(@"Could not migrate preference file from %@ to %@.", oldPreferencesFile, newPreferencesFile);
+        }
+    }
 }
