f9d23e6a9de9b06f1c5f883220f586e53f01e4b0
Nikolaj Schumacher <git@nschum.de>
2014-01-29 09:30:42 +0100

    #244: Remove shadow from box and make border thinner.

--- Hive/Views/HIBox.m
+++ Hive/Views/HIBox.m
@@ -8,33 +8,29 @@
 
 #import "HIBox.h"
 
+#import "NSColor+Hive.h"
+
 @implementation HIBox
 
 - (id)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code here.
+        [self setUpBox];
     }
-    
     return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect {
-    NSBezierPath *p = [NSBezierPath bezierPathWithRoundedRect:self.bounds xRadius:5 yRadius:5];
-    
-    [[NSColor whiteColor] setFill];
-    [p fill];
-    [RGB(195,195,195) set];
-    [p stroke];
-    
-    NSBezierPath *sP = [NSBezierPath bezierPath];
-    NSRect frame = self.bounds;
-    [sP moveToPoint:NSMakePoint(1, frame.size.height-3)];
-    [sP curveToPoint:NSMakePoint(5, frame.size.height-1) controlPoint1:NSMakePoint(5, frame.size.height-1) controlPoint2:NSMakePoint(5, frame.size.height-1)];
-    [sP lineToPoint:NSMakePoint(frame.size.width-5, frame.size.height-1)];
-    [sP curveToPoint:NSMakePoint(frame.size.width-1, frame.size.height-3) controlPoint1:NSMakePoint(frame.size.width-5, frame.size.height-1) controlPoint2:NSMakePoint(frame.size.width-5, frame.size.height-1)];
-    [[NSColor colorWithCalibratedWhite:0 alpha:0.35] set];
-    [sP stroke];
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self setUpBox];
+}
+
+- (void)setUpBox {
+    [self setWantsLayer:YES];
+    self.layer.borderWidth = 1.0;
+    self.layer.cornerRadius = 5.0;
+    self.layer.borderColor = RGB(195,195,195).hiNativeColor;
+    self.layer.backgroundColor = [NSColor whiteColor].hiNativeColor;
 }
 
 @end
