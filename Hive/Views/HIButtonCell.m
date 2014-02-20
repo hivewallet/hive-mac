6d39322d2e06ed3362a3b70b2ee62584df461296
Jakub Suder <jakub.suder@gmail.com>
2014-02-20 17:02:44 +0100

    [#274] HIButtonCell cleanup

--- Hive/Views/HIButtonCell.m
+++ Hive/Views/HIButtonCell.m
@@ -15,14 +15,24 @@ static const float PADDING_Y = 0.0;
 @implementation HIButtonCell
 
 - (void)drawBezelWithFrame:(NSRect)frame inView:(NSView *)controlView {
+    double cornerRadius = 5.0;
+    double midPoint = (cornerRadius - 1.0) / 2 + 1.0;
+
     NSBezierPath *p = [NSBezierPath bezierPathWithRoundedRect:frame xRadius:5 yRadius:5];
+
     if (self.isHighlighted) {
         NSBezierPath *sP = [NSBezierPath bezierPath];
         
-        [sP moveToPoint:NSMakePoint(1, 3)];
-        [sP curveToPoint:NSMakePoint(5, 1) controlPoint1:NSMakePoint(5, 1) controlPoint2:NSMakePoint(5, 1)];
-        [sP lineToPoint:NSMakePoint(frame.size.width-5, 1)];
-        [sP curveToPoint:NSMakePoint(frame.size.width-1, 3) controlPoint1:NSMakePoint(frame.size.width-5, 1) controlPoint2:NSMakePoint(frame.size.width-5, 1)];
+        [sP moveToPoint:NSMakePoint(1, midPoint)];
+        [sP curveToPoint:NSMakePoint(cornerRadius, 1)
+           controlPoint1:NSMakePoint(cornerRadius, 1)
+           controlPoint2:NSMakePoint(cornerRadius, 1)];
+
+        [sP lineToPoint:NSMakePoint(frame.size.width - cornerRadius, 1)];
+        [sP curveToPoint:NSMakePoint(frame.size.width - 1, midPoint)
+           controlPoint1:NSMakePoint(frame.size.width - cornerRadius, 1)
+           controlPoint2:NSMakePoint(frame.size.width - cornerRadius, 1)];
+
         [[NSColor colorWithCalibratedWhite:0 alpha:0.35] set];
         [RGB(237, 237, 237) setFill];
         [p fill];
@@ -30,19 +40,14 @@ static const float PADDING_Y = 0.0;
         
         controlView.layer.shadowColor = [[NSColor whiteColor] hiNativeColor];
     } else {
-//        [RGB(183, 183, 183) set];
-//        [sP stroke];
-        
         NSGradient *g = [[NSGradient alloc] initWithColors:@[RGB(250,250,250), RGB(237, 237, 237)]];
         [g drawInBezierPath:p angle:90];
         controlView.layer.shadowColor = [[NSColor blackColor] hiNativeColor];
     }
-    
+
     p.lineWidth = 0.5;    
     [RGB(128, 128, 128) set];
     [p stroke];
-    
-    
 }
 
 - (NSRect)drawTitle:(NSAttributedString *)title withFrame:(NSRect)frame inView:(NSView *)controlView {
@@ -53,19 +58,14 @@ static const float PADDING_Y = 0.0;
     
     NSDictionary *attrs = @{NSFontAttributeName: [NSFont fontWithName:@"Helvetica" size:13], NSForegroundColorAttributeName: RGB(63, 63, 63), NSShadowAttributeName: sh};
     NSSize size = [self.title sizeWithAttributes:attrs];
-    NSRect drawRect;
-//    if (self.isHighlighted)
-//    {
-//        drawRect = NSMakeRect(frame.origin.x + (frame.size.width - size.width) / 2.0,
-//                              frame.origin.y + 1 + (frame.size.height - size.height) / 2.0, size.width, size.height);
-//    }
-//    else
-//    {
-        drawRect = NSMakeRect(frame.origin.x + (frame.size.width - size.width) / 2.0,
-                              frame.origin.y - 1 + (frame.size.height - size.height) / 2.0, size.width, size.height);        
-//    }
+
+
+    NSRect drawRect = NSMakeRect(frame.origin.x + (frame.size.width - size.width) / 2.0,
+                                 frame.origin.y - 1 + (frame.size.height - size.height) / 2.0,
+                                 size.width, size.height);
+
     [self.title drawAtPoint:drawRect.origin withAttributes:attrs];
-    
+
     return drawRect;
 }
 
