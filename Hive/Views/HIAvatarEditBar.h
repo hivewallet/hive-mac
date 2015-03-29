7e2c75e4a95bf36ef1e3f4183529853672b06076
Jakub Suder <jakub.suder@gmail.com>
2015-03-29 04:20:50 +0200

    [#409] make the “edit” bar on avatar clickable

---
+++ Hive/Views/HIAvatarEditBar.h
@@ -0,0 +1,13 @@
+//
+//  HIAvatarEditBar.h
+//  Hive
+//
+//  Created by Kuba Suder on 29.03.2015.
+//  Copyright (c) 2015 Hive Developers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HIAvatarEditBar : NSTextField
+
+@end
---
+++ Hive/Views/HIAvatarEditBar.m
@@ -0,0 +1,17 @@
+//
+//  HIAvatarEditBar.m
+//  Hive
+//
+//  Created by Kuba Suder on 29.03.2015.
+//  Copyright (c) 2015 Hive Developers. All rights reserved.
+//
+
+#import "HIAvatarEditBar.h"
+
+@implementation HIAvatarEditBar
+
+- (NSView *)hitTest:(NSPoint)aPoint {
+    return nil;
+}
+
+@end
